103fceb75d3a494b1bc413ed018a5ec87b2cb263
thenaoshimaboy@gmail.com <thenaoshimaboy@gmail.com>
2021-03-12 08:59:27 +0100

    debut firebase

--- app/src/main/java/helloandroid/m2dl/earthquake/game_menu/MainMenu.java
+++ app/src/main/java/helloandroid/m2dl/earthquake/game_menu/MainMenu.java
@@ -2,9 +2,14 @@ package helloandroid.m2dl.earthquake.game_menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import helloandroid.m2dl.earthquake.R;
 import helloandroid.m2dl.earthquake.game.GameActivity;
@@ -22,4 +27,17 @@ public class MainMenu extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+    public void test(View view){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+        System.out.println(FirebaseDatabase.getInstance().getApp().getName());
+
+        TextView txtView = (TextView) ((Activity)this).findViewById(R.id.textView);
+        txtView.setText("test");
+    }
+
+
 }
