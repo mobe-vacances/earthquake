b9084f64028f485c6eab6812f91372d9b670b2f3
Nicolas ANDRÉ <45142835+iomega11@users.noreply.github.com>
2021-03-14 19:31:55 +0100

    Added simple score view with highest score handling

--- app/src/main/java/helloandroid/m2dl/earthquake/game_menu/GameOver.java
+++ app/src/main/java/helloandroid/m2dl/earthquake/game_menu/GameOver.java
@@ -3,28 +3,68 @@ package helloandroid.m2dl.earthquake.game_menu;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import helloandroid.m2dl.earthquake.R;
+import helloandroid.m2dl.earthquake.game.state.GameState;
 
 public class GameOver extends AppCompatActivity {
+
+    int highestScore;
+
+    int currentScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+        loadHighestScore();
+        loadCurrentScore();
+        updateHighestScore();
+        showScore();
         fillScoresTable();
+    }
+
+    private void loadHighestScore() {
+        SharedPreferences preferences = getSharedPreferences("score", Context.MODE_PRIVATE);
+        highestScore = preferences.getInt("highest_score", 0);
+    }
+
+    private void loadCurrentScore() {
+        currentScore = GameState.getScore();
+    }
+
+    private void updateHighestScore() {
+        if (currentScore > highestScore) {
+            highestScore = currentScore;
+        }
+    }
+
+    private void saveHighestScore() {
+        SharedPreferences preferences = getSharedPreferences("score", Context.MODE_PRIVATE);
+        SharedPreferences.Editor localScoreEditor = preferences.edit();
+        localScoreEditor.putInt("highest_score", highestScore);
+        localScoreEditor.apply();
+    }
+
+    private void showScore() {
+        TextView currentScoreView = findViewById(R.id.currentScore);
+        currentScoreView.setText(currentScoreView.getText().toString() + currentScore);
+        TextView highestScoreView = findViewById(R.id.highestScore);
+        highestScoreView.setText(highestScoreView.getText().toString() + highestScore);
     }
 
     private void fillScoresTable() {
         LinearLayout scoresTable = findViewById(R.id.scoresLayout);
         ConstraintLayout scoresLayout = (ConstraintLayout) scoresTable.getChildAt(0);
 
-        for(int i = 0; i < scoresLayout.getChildCount(); i++) {
+        for (int i = 0; i < scoresLayout.getChildCount(); i++) {
             LinearLayout line = (LinearLayout) scoresLayout.getChildAt(i);
             ConstraintLayout cr = (ConstraintLayout) line.getChildAt(0);
             TextView name = (TextView) cr.getChildAt(1);
@@ -35,8 +75,15 @@ public class GameOver extends AppCompatActivity {
         }
     }
 
-    public void back(View view){
+    public void back(View view) {
         Intent intent = new Intent(GameOver.this, MainMenu.class);
         startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        saveHighestScore();
+        super.onDestroy();
     }
 }
