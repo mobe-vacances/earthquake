a896a92bfd6979734ff68acba649384957098d8e
NinaNekonoran <franzese.rozen@gmail.com>
2021-03-17 18:56:18 +0100

    Fix

--- app/src/main/java/helloandroid/m2dl/earthquake/game_menu/MainMenu.java
+++ app/src/main/java/helloandroid/m2dl/earthquake/game_menu/MainMenu.java
@@ -1,11 +1,9 @@
 package helloandroid.m2dl.earthquake.game_menu;
 
-import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
