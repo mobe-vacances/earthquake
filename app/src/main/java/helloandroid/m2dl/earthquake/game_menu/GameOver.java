24792af4ea967a45e6488cf12bc18d248ffb837b
Nicolas ANDRÉ <45142835+iomega11@users.noreply.github.com>
2021-03-18 19:02:49 +0100

    Refonte du tableau des scores

--- app/src/main/java/helloandroid/m2dl/earthquake/game_menu/GameOver.java
+++ app/src/main/java/helloandroid/m2dl/earthquake/game_menu/GameOver.java
@@ -1,16 +1,27 @@
 package helloandroid.m2dl.earthquake.game_menu;
 
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
+import android.graphics.Movie;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import helloandroid.m2dl.earthquake.R;
+import helloandroid.m2dl.earthquake.game.GameConstants;
 import helloandroid.m2dl.earthquake.game.database.HighscoreHandler;
 import helloandroid.m2dl.earthquake.game.database.WorldScoresHandler;
 import helloandroid.m2dl.earthquake.game.state.GameState;
@@ -20,6 +31,31 @@ import static java.lang.Math.min;
 
 public class GameOver extends AppCompatActivity {
 
+    private class ScoreAdapter extends ArrayAdapter {private Context mContext;
+
+        private List<Score> scores;
+
+        public ScoreAdapter(@NonNull Context context, List<Score> list) {
+            super(context, 0 , list);
+            mContext = context;
+            scores = list;
+            System.out.println(scores.size());
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View listItem = convertView;
+            if(listItem == null) {
+                listItem = LayoutInflater.from(mContext).inflate(R.layout.score_item, parent, false);
+            }
+
+            Score score = scores.get(position);
+            ((TextView) listItem.findViewById(R.id.name)).setText(score.getName());
+            ((TextView) listItem.findViewById(R.id.score)).setText(score.getValue() + "");
+            return listItem;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +63,7 @@ public class GameOver extends AppCompatActivity {
         setContentView(R.layout.activity_game_over);
 
         Score score = new Score(getUsername(), GameState.getScore());
+        System.out.println(getUsername());
         HighscoreHandler.updateHighScore(score);
 
         showScore();
@@ -42,7 +79,7 @@ public class GameOver extends AppCompatActivity {
     }
 
     private String getUsername() {
-        return getPreferences( MODE_PRIVATE).getString("username", getResources().getString(R.string.default_player_name));
+        return getSharedPreferences(GameConstants.APP_SHARED_PREFERENCES_KEY, MODE_PRIVATE).getString("username", getResources().getString(R.string.default_player_name));
     }
 
     private void showScore() {
@@ -51,24 +88,11 @@ public class GameOver extends AppCompatActivity {
     }
 
     public void showScoresTable() {
-        ConstraintLayout constraintLayout = findViewById(R.id.scoreConstraint);
-        for (int i = 0; i < constraintLayout.getChildCount(); i++) {
-            constraintLayout.getChildAt(i).setVisibility(View.GONE);
-        }
-
         List<Score> worldScores = WorldScoresHandler.getScores();
+        ListView listView = findViewById(R.id.scores);
+        ScoreAdapter arrayAdapter = new ScoreAdapter(this, worldScores);
+        listView.setAdapter(arrayAdapter);
 
-        for (int i = 0; i < min(worldScores.size(), constraintLayout.getChildCount()); i++) {
-            try {
-                System.out.println(worldScores.get(i).getName() + " - " + worldScores.get(i).getValue());
-                LinearLayout linearLayout = (LinearLayout) constraintLayout.getChildAt(i);
-                linearLayout.setVisibility(View.VISIBLE);
-                ((TextView) linearLayout.getChildAt(1)).setText(worldScores.get(i).getName());
-                ((TextView) linearLayout.getChildAt(2)).setText(worldScores.get(i).getValue() + "");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
     }
 
 
